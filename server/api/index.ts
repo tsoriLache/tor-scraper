feb42f357c8fb64b523eeb9577d8c51989cd731e
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-08 11:15:17 +0200

    move api to seprate service in server-side

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,15 +1,12 @@
 const PORT = 5000;
-import axios from 'axios';
-import cheerio from 'cheerio';
+
 import express from 'express';
 const app = express();
 import cors from 'cors';
 app.use(cors());
 
-app.get('/ping',(req, res)=>{  
-    console.log('p');
-      
-    res.json('pong')
+app.get('/',(req, res)=>{        
+    res.json('This is my web-scraper');
 })
 
 app.listen(PORT, () => {
