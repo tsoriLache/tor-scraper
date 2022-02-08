4402023de23c24bdef9918d1b752c2b84d250eb2
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-08 11:38:51 +0200

    splitSignature
    extract author and date from one string

--- server/scrape-worker/helpers/helpers.ts
+++ server/scrape-worker/helpers/helpers.ts
@@ -12,3 +12,10 @@ const format = (elem: any) => {
   return elem.text().replace(/[\n\t\r]/g, '');
 };
 
+const splitSignature = (signature: string) => {
+  if (typeof signature.split('Posted by ')[1] === 'undefined') return {};
+  const split = signature.split('Posted by ')[1].split(' at ');
+  const author = split[0];
+  const date = split[1].split(', ')[0];
+  return { author, date };
+};
