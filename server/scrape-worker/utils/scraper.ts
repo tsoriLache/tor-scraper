17598ca81d462f3439baafc7a0f57a08f7cb1f03
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-08 11:31:35 +0200

    first scrape
    get links to all the pastes in the page

--- server/scrape-worker/utils/scraper.ts
+++ server/scrape-worker/utils/scraper.ts
@@ -11,3 +11,17 @@ const BASE_URL =
   'http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all?page=';
 const CSS_SELECTOR = '.col-sm-12';
 
+const getLinks = async (cssSelector: string, url: string) => {
+  try {
+    const html = await getHtml(url);
+    const $ = cheerio.load(html);
+    let links: {}[] = [];
+    $(`${cssSelector}`, html).each((_: number, cheerioElement: any) => {
+      const detailedLink = $(cheerioElement).find('a').attr('href');
+      links = [...links, detailedLink];
+    });
+    return links;
+  } catch (err) {
+    console.log(err);
+  }
+};
