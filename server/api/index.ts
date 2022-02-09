3b3dbed2a44fc87c993941d88f92a35dc3342a3f
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-09 18:41:25 +0200

    top 10 recent pastes end-point

--- server/api/index.ts
+++ server/api/index.ts
@@ -3,12 +3,23 @@ const PORT = 5000;
 import express from 'express';
 const app = express();
 import cors from 'cors';
+import { pool } from './db/config';
 app.use(cors());
 
-app.get('/',(req, res)=>{        
-    res.json('This is my web-scraper');
-})
+app.get('/', (req, res) => {
+  res.json('This is my web-scraper');
+});
+
+app.get('/recent', (req, res) => {
+  pool.query('SELECT * FROM pastes limit 10', (err: any, data: any) => {
+    if (err) console.log(err);
+    else {
+      console.log(data);
+      res.json(data);
+    }
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`server running on PORT ${PORT}`);
-  });
+  console.log(`server running on PORT ${PORT}`);
+});
