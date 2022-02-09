4b3d90254f885fd03b015fca7b9b0b85fa2893a5
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-09 18:39:08 +0200

    MU dashboard

---
+++ client/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Dashboard from './MU/Dashboard';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
