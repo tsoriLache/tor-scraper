b5cb45746e074d35e2ade3957a55299b6106006c
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-11 12:42:44 +0200

    searchKeywords
    search all kw

--- server/api/keywords/keywords.ts
+++ server/api/keywords/keywords.ts
@@ -1,3 +1,18 @@
+const searchKeywords = async (keywords: string[], clientId: string) => {
+  const results = await Promise.all(
+    keywords.map(async (keyword) => await searchKeyword(keyword))
+  );
+  return sendEventsToClient(organizedResult(keywords, results), clientId);
+};
+
+const organizedResult = (keywords: string[], results: any) => {
+  const organizedResult: any = [];
+  keywords.forEach((keyword: string, i: number) =>
+    organizedResult.push({ [keyword]: results[i] })
+  );
+  return organizedResult;
+};
+
 const searchKeyword = async (keyword: string) => {
   try {
     const results = await Paste.findAll({
