6c8320cb12d1377a09a52b394f6c7d6e9358cb28
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-13 16:57:52 +0200

    routing

--- client/src/MU/listItems.tsx
+++ client/src/MU/listItems.tsx
@@ -8,21 +8,28 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PeopleIcon from '@mui/icons-material/People';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import LayersIcon from '@mui/icons-material/Layers';
-import AssignmentIcon from '@mui/icons-material/Assignment';
 
-export const mainListItems = (
+export const mainListItems = (setRoute: any) => (
   <React.Fragment>
-    <ListItemButton>
+    <ListItemButton
+      onClick={() => {
+        setRoute('recent');
+      }}
+    >
       <ListItemIcon>
         <DashboardIcon />
       </ListItemIcon>
       <ListItemText primary="Dashboard" />
     </ListItemButton>
-    <ListItemButton>
+    <ListItemButton
+      onClick={() => {
+        setRoute('notify');
+      }}
+    >
       <ListItemIcon>
         <PeopleIcon />
       </ListItemIcon>
-      <ListItemText primary="Customers" />
+      <ListItemText primary="Notifications" />
     </ListItemButton>
     <ListItemButton>
       <ListItemIcon>
@@ -30,31 +37,15 @@ export const mainListItems = (
       </ListItemIcon>
       <ListItemText primary="Reports" />
     </ListItemButton>
-    <ListItemButton onClick={() => {}}>
+    <ListItemButton
+      onClick={() => {
+        setRoute('keywords');
+      }}
+    >
       <ListItemIcon>
         <LayersIcon />
       </ListItemIcon>
       <ListItemText primary="By Keywords" />
-    </ListItemButton>
-  </React.Fragment>
-);
-
-export const secondaryListItems = (
-  <React.Fragment>
-    <ListSubheader component="div" inset>
-      Saved reports
-    </ListSubheader>
-    <ListItemButton>
-      <ListItemIcon>{/* <AssignmentIcon /> */}</ListItemIcon>
-      <ListItemText primary="Current month" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>{/* <AssignmentIcon /> */}</ListItemIcon>
-      <ListItemText primary="Last quarter" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>{/* <AssignmentIcon /> */}</ListItemIcon>
-      <ListItemText primary="Year-end sale" />
     </ListItemButton>
   </React.Fragment>
 );
