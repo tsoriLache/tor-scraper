b64197c95855e2a884e39d594247c0f0bbbe1931
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-13 12:43:44 +0200

    check and get new results

--- server/api/keywords/checker.ts
+++ server/api/keywords/checker.ts
@@ -1,15 +1,29 @@
-const getOnlyNewPastes = (oldarr: any, newarr: any): any => {
+const getOnlyNewPastes = (oldArr: any, newArr: any): any => {
   const comparison = (a: any, b: any) => a.id !== b.id;
-  return newarr.filter((n: any) => oldarr.every((o: any) => comparison(o, n)));
+  return newArr.filter((n: any) => oldArr.every((o: any) => comparison(o, n)));
 };
 
-const checkAllKeysForNewPastes = (OLDRESULT: any, RESULT: any) => {
-  return RESULT.map((keywordObj: any, i: number) => {
+const getAllKeysForNewPastes = (oldResult: any, result: any) => {
+  return result.map((keywordObj: any, i: number) => {
     const key: string = Object.keys(keywordObj)[0];
+    const newPastes = getOnlyNewPastes(
+      oldResult[i][`${key}`],
+      keywordObj[`${key}`]
+    );
     return {
-      [key]: getOnlyNewPastes(OLDRESULT[i][`${key}`], keywordObj[`${key}`]),
+      [key]: newPastes,
     };
   });
 };
 
-export default checkAllKeysForNewPastes;
+const checkAllKeysForNewPastes = (
+  result: { [x: string]: string[] }[]
+): boolean => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return !result.every(
+    (resultByKey: any) => Object.values(resultByKey)[0].length === 0
+  );
+};
+
+export { getAllKeysForNewPastes, checkAllKeysForNewPastes };
