43105fbe2357cbf3fddba69c0ba8528a5a4befd6
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-13 12:46:09 +0200

    sendNotificationToClient()

--- server/api/routers/keywordsRouter.ts
+++ server/api/routers/keywordsRouter.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { searchForKeywords } from '../keywords/keywords';
-import { ClientRequest } from '../types';
+import { ClientRequest, Result } from '../types';
+import {
+  getAllKeysForNewPastes,
+  checkAllKeysForNewPastes,
+} from '../keywords/checker';
 const router = express.Router();
 
 let clients: any = [];
-let searchRequests: ClientRequest[] = [];
+// let searchRequests: ClientRequest[] = [];
 let pastes: any[][] = [];
 
 router.get('/:clientId', (req: any, res: any, next: any) => {
@@ -34,19 +38,32 @@ router.get('/:clientId', (req: any, res: any, next: any) => {
   });
 });
 
-router.post('/:clientId', (req: any, res: any, next: any) => {
+let results: Result;
+router.post('/:clientId', async (req: any, res: any, next: any) => {
+  results = { old: [], neww: [] };
   const { clientId } = req.params;
   const { keywords } = req.body;
 
-  const crIndex = searchRequests.findIndex((cr) => cr.clientId === clientId);
-  if (crIndex >= 0) searchRequests[crIndex].keywords = keywords;
-  else searchRequests.push({ clientId, keywords });
+  // const crIndex = searchRequests.findIndex((cr) => cr.clientId === clientId);
+  // if (crIndex >= 0) searchRequests[crIndex].keywords = keywords;
+  // else searchRequests.push({ clientId, keywords });
+  // searchRequests.find((request) => {
+  //   request.clientId === clientId;
+  // }).keywords;
 
-  searchForKeywords(searchRequests);
-
-  setInterval(() => {
-    searchForKeywords(searchRequests);
-  }, 120000);
+  const newResult = await searchForKeywords(keywords);
+  sendEventsToClient(newResult, clientId);
+  setInterval(async () => {
+    console.log('2min');
+    const newResult = await searchForKeywords(keywords);
+    results.old = results.neww;
+    results.neww = newResult;
+    if (results.old.length > 0) {
+      const newPastes = getAllKeysForNewPastes(results.old, results.neww);
+      if (checkAllKeysForNewPastes(newPastes))
+        sendNotificationToClient(newPastes, newResult, clientId);
+    }
+  }, 12000);
   res.json('searching...');
 });
 
@@ -59,4 +76,18 @@ export function sendEventsToClient(pastes: any, clientId: any) {
   }
 }
 
+export function sendNotificationToClient(
+  pastes: any,
+  newResult: any,
+  clientId: any
+) {
+  try {
+    const client = clients.find(({ id }: any) => id === clientId);
+    client.res.write(
+      `data: ${JSON.stringify({ notify: pastes, data: newResult })}\n\n`
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
 export default router;
