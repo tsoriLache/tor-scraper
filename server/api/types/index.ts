4c00e4c38aecceb00d460bbbf55522d592066ffe
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2022-02-13 12:41:14 +0200

    cleaning code

--- server/api/types/index.ts
+++ server/api/types/index.ts
@@ -2,3 +2,8 @@ export interface ClientRequest {
   clientId: string;
   keywords: string[];
 }
+
+export interface Result {
+  old: { [x: string]: string[] }[];
+  neww: { [x: string]: string[] }[];
+}
